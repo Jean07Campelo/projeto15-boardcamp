746ef9937f8e7a9806bbfcbff653089e9740d09a
Jean Campelo <jean93campelo@gmail.com>
2022-10-01 14:46:24 -0300

    Validação se existe uma categoria com mesmo nome cadastrada

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -5,7 +5,7 @@ const categorieSchema = joi.object({
   name: joi.string().required().empty(" "),
 });
 
-function RegisterNewCategorie(req, res) {
+async function RegisterNewCategorie(req, res) {
   const { name } = req.body;
 
   const validationCategorie = categorieSchema.validate(req.body, {
@@ -18,6 +18,17 @@ function RegisterNewCategorie(req, res) {
     res.status(400).send(errors);
   }
 
+  //validation if category is registered
+  const categorieExisted = await connection.query(
+    "SELECT * FROM categories WHERE name = $1;",
+    [name]
+  );
+
+  if (categorieExisted.rows.length > 0) {
+    return res.status(409).send(`The categorie "${name}" is ready registered`);
+  }
+
+  //register new category
   connection.query("INSERT INTO categories (name) VALUES ($1);", [name]);
 
   res.sendStatus(201);
