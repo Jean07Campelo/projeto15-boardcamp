8a10de6e7fca1ada0dfd4b8a3d7628fac33addeb
Jean Campelo <jean93campelo@gmail.com>
2022-10-02 10:17:11 -0300

    Consulta de todos clientes cadastrados

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,5 +1,8 @@
-async function GetCustomers (req, res) {
-    
-};
+import connection from "../database.js";
+
+async function GetCustomers(req, res) {
+  const customers = await connection.query("SELECT * FROM customers;");
+  res.status(200).send(customers.rows);
+}
 
 export { GetCustomers };
