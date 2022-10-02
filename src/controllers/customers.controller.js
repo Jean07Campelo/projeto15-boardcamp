e3d7b3c6b550b85c7d59ddb104b1ece24fd6f575
Jean Campelo <jean93campelo@gmail.com>
2022-10-02 10:13:26 -0300

    Configuração da função para buscar clientes cadastrados

---
+++ src/controllers/customers.controller.js
@@ -0,0 +1,5 @@
+async function GetCustomers (req, res) {
+    
+};
+
+export { GetCustomers };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import * as categoriesController from "../controllers/categories.controller.js";
 import * as gamesController from "../controllers/games.controller.js";
+import * as customersController from "../controllers/customers.controller.js";
 
 const router = express.Router();
 
@@ -10,5 +11,7 @@ router.get("/categories", categoriesController.GetCategories);
 
 router.post("/games", gamesController.RegisterNewGame);
 router.get("/games", gamesController.GetGames);
+
+router.get("/customers", customersController.GetCustomers);
 
 export default router;
