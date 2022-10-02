c62a3ffaf7962d6520ab46183ccdfba89191715d
Jean Campelo <jean93campelo@gmail.com>
2022-10-02 10:30:04 -0300

    Restringindo cadastro de telefone com 10 ou 11 caracteres

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,12 +1,33 @@
+import joi from "joi";
 import connection from "../database.js";
+
+const clientSchema = joi.object({
+  name: joi.string().required().empty(" "),
+  phone: joi.string().required().min(10).max(11),
+  cpf: joi.string().required(),
+  birthday: joi.string().required(),
+});
 
 async function GetCustomers(req, res) {
   const customers = await connection.query("SELECT * FROM customers;");
   res.status(200).send(customers.rows);
-};
+}
 
-async function RegisterNewClient (req, res) {
+async function RegisterNewClient(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
 
-};
+  const validationNewClient = clientSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validationNewClient.error) {
+    const errors = validationNewClient.error.details.map(
+      (detail) => detail.message
+    );
+    return res.status(400).send(errors);
+  }
+
+  res.sendStatus(201);
+}
 
 export { GetCustomers, RegisterNewClient };
