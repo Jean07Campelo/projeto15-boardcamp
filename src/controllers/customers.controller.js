75e5bd68d9d86b8393d5f8cc7a77b483ac002564
Jean Campelo <jean93campelo@gmail.com>
2022-10-02 16:49:33 -0300

    Verificando se existe o mesmo cpf cadastrado no banco, caso sim, recusa o novo cadastro

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -6,7 +6,7 @@ const clientSchema = joi.object({
   name: joi.string().required().empty(" "),
   phone: joi.string().required().min(10).max(11),
   cpf: joi.string().required().min(11).max(11),
-  birthday: joi.date().less("2022-12-31"),
+  birthday: joi.string().required(),
 });
 
 async function GetCustomers(req, res) {
@@ -31,10 +31,26 @@ async function RegisterNewClient(req, res) {
   //validation birthday
   const birthdayIsValid = dayjs(birthday, "YYYY-MM-DD").isValid();
   if (!birthdayIsValid) {
-    return res.status(400).send(`${birthday} is invalid`)
+    return res.status(400).send(`${birthday} is invalid`);
   }
-  
 
+  const birthdayFormat = dayjs(birthday).format("YYYY-MM-DD");
+
+  //validation if cpf is registered
+  const documentIsRegistered = await connection.query(
+    "SELECT * FROM customers WHERE cpf = $1;",
+    [cpf]
+  );
+
+  if (documentIsRegistered.rows.length > 0) {
+    return res.status(409).send(`The document ${cpf} is already registered`);
+  }
+
+  //register new client
+  await connection.query(
+    `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`,
+    [name, phone, cpf, birthdayFormat]
+  );
 
   res.sendStatus(201);
 }
