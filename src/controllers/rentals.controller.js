6789a75cdba760bbc79e5a413e1f4dad3d556473
Jean Campelo <jean93campelo@gmail.com>
2022-10-03 09:20:30 -0300

    Validação dos dias de solicitação de aluguel

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,7 +1,24 @@
+import joi from "joi";
 import connection from "../database.js";
+
+const solicitationRentalSchema = joi.object({
+  customerId: joi.number().required(),
+  gameId: joi.number().required(),
+  daysRented: joi.number().required().positive(),
+});
 
 async function GetRentals(req, res) {
   const { customerId, gameId, daysRented } = req.body;
+
+  const validationRental = solicitationRentalSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (validationRental.error) {
+    const errors = validationRental.error.details.map(
+      (detail) => detail.message
+    );
+    return res.status(400).send(errors);
+  }
 
   //validation customerId
   const userExisting = await connection.query(
@@ -18,7 +35,7 @@ async function GetRentals(req, res) {
     [gameId]
   );
   if (gameExisting.rows.length === 0) {
-    return res.status(400).send(`Do not exists game with id "${customerId}"`);
+    return res.status(400).send(`Do not exists game with id "${gameId}"`);
   }
 
   res.sendStatus(201);
