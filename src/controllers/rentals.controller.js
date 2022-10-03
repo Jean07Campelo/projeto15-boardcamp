2b99362d7c036a258c24235946a6955eb53abc6e
Jean Campelo <jean93campelo@gmail.com>
2022-10-03 09:15:09 -0300

    Verifica se id passado pertence a um jogo cadastrado

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -3,13 +3,22 @@ import connection from "../database.js";
 async function GetRentals(req, res) {
   const { customerId, gameId, daysRented } = req.body;
 
+  //validation customerId
   const userExisting = await connection.query(
     "SELECT * FROM customers WHERE id = $1;",
     [customerId]
   );
-
   if (userExisting.rows.length === 0) {
-    return res.status(400).send(`Do not exists cliente with id "${customerId}"`);
+    return res.status(400).send(`Do not exists client with id "${customerId}"`);
+  }
+
+  //validation gameId
+  const gameExisting = await connection.query(
+    "SELECT * FROM customers WHERE id = $1;",
+    [gameId]
+  );
+  if (gameExisting.rows.length === 0) {
+    return res.status(400).send(`Do not exists game with id "${customerId}"`);
   }
 
   res.sendStatus(201);
