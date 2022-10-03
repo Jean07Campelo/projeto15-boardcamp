10d767687b1bd4168f177ed89f47d364f158877a
Jean Campelo <jean93campelo@gmail.com>
2022-10-03 09:13:23 -0300

    Verifica se id passado pertence a um cliente válido

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,5 +1,18 @@
-async function GetRentals (req, res) {
+import connection from "../database.js";
 
-};
+async function GetRentals(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
 
-export { GetRentals }
+  const userExisting = await connection.query(
+    "SELECT * FROM customers WHERE id = $1;",
+    [customerId]
+  );
+
+  if (userExisting.rows.length === 0) {
+    return res.status(400).send(`Do not exists cliente with id "${customerId}"`);
+  }
+
+  res.sendStatus(201);
+}
+
+export { GetRentals };
